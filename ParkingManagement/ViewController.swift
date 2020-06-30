84a4c8f28b04b89ac857472e93e08a5f92fc3d55
abhishekrawat <abhishek.rawat@evontech.com>
2020-06-30 14:21:16 +0530

    Initial Commit

---
+++ ParkingManagement/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParkingManagement
+//
+//  Created by abhishekrawat on 30/06/20.
+//  Copyright © 2020 Test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
